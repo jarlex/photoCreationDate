beefbaf41eb22f50a69808a6b4b51692c309d4c4
Andres Ruiz Carrasco <jarlex.hack@gmail.com>
2019-04-14 13:01:06 +0200

    Separe Lib + improves
    - Copy exif data
    - Uptade date to earlier date

--- test_main.py
+++ test_main.py
@@ -1,7 +1,6 @@
 from Mierdecillas.photoCreationDate import lib as photo_creation_date
 import argparse
 
-# Main script
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
 parser.add_argument("path", type=str, help="work directory")
@@ -14,4 +13,8 @@ parser.add_argument('--date',
                     help='date to set, if not present get the last modified date')
 args = parser.parse_args()
 
-photo_creation_date.change_full_directory_modification_time(args.path, args.date)
+print(photo_creation_date.get_creation_photo_time(args.path))
+print(photo_creation_date.get_creation_time(args.path))
+print(photo_creation_date.get_modification_time(args.path))
+
+print(photo_creation_date.older_date(args.path))
